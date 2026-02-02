00164395975891e6f6d6570cd1969767044591b4
matei1234-lgtm <gorgan.natanail@gmail.com>
2026-02-02 16:37:35 +0200

    Update valentineName and pageTitle in config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "Cezi",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💝",
+    pageTitle: "Vrei sa fii valentinul meu?🤗💕💕💕💕​",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
